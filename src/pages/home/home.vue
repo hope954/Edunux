d6f11821603ba0841207709f589d02cf759e1228
yan-李睿彤 <14868321+yan-li-ruitong@user.noreply.gitee.com>
2025-05-08 18:47:26 +0800

    fix:可视化大屏暂时云托管

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -3,8 +3,11 @@
         <!-- <h1 style="font-size:32px;font-weight:600;">双创满意度数据展示</h1> -->
         <!-- <div class="time">{{ currentTime }}</div> -->
         <!-- <img src="../../assets/image/mes.1.png" alt=""> -->
-        <iframe width=1600 height=900 src="http://10.198.70.50:10000/danyhope954/" frameborder=0 allowfullscreen=true></iframe>
-    </div>
+         <!-- 需要打开山海鲸才能访问，还没有托管到云服务上 -->
+        <!-- <iframe width=1600 height=900 src="http://10.198.70.50:10000/danyhope954/" frameborder=0 allowfullscreen=true></iframe> -->
+        <!-- 这个是托管的版本可以直接在线访问https://hosting.shanhaibi.com/n24etvcglz7i/ -->
+        <iframe width=1600 height=900 src="https://hosting.shanhaibi.com/n24etvcglz7i/" frameborder=0 allowfullscreen=true></iframe>
+      </div>
   </template>
   
   <script>
